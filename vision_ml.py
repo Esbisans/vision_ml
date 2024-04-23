158f314e7a3cffc6aa25b54981a3cbd5d7267841
Esbisans <193281@ids.upchiapas.edu.mx>
2024-04-23 13:35:29 -0600

    update real time camera

--- vision_ml.py
+++ vision_ml.py
@@ -1,12 +1,16 @@
-import streamlit as st
+import av
 import cv2 
-import mediapipe as mp
+import queue
 import numpy as np
-from mediapipe.framework.formats import landmark_pb2
-from mediapipe.tasks import python
+import streamlit as st
+import mediapipe as mp
 from mediapipe import solutions
+from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from mediapipe.framework.formats import landmark_pb2
 from streamlit_option_menu import option_menu
+from streamlit_webrtc import webrtc_streamer
+from typing import List, NamedTuple
 
 
 def visualize(image, detection_result) -> np.ndarray:
@@ -88,7 +92,10 @@ def draw_landmarks_on_image(rgb_image, detection_result):
 
   return annotated_image
 
-def object_detection(camera_index):
+def object_detection(frame: av.VideoFrame) -> av.VideoFrame:
+    image = frame.to_ndarray(format="bgr24")
+    rgb_frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
 
     model_path = 'models/efficientdet_lite0.tflite'
 
@@ -97,43 +104,20 @@ def object_detection(camera_index):
                                        score_threshold=0.5)
     detector = vision.ObjectDetector.create_from_options(options)
 
-    cap = cv2.VideoCapture(camera_index)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
 
-    if not cap.isOpened():
-        st.subheader("camera not available")
-        st.caption("Please change the camera channel and try again")
-        ok_button = st.button("Ok")
-        return
+    detection_result = detector.detect(mp_image)
+    image_copy = np.copy(mp_image.numpy_view())
+    annotated_image = visualize(image_copy, detection_result)
+    rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
 
 
-    stop_button_pressed = st.button("Stop camera", type="primary")
-    frame_placeholder = st.empty()
+    return av.VideoFrame.from_ndarray(rgb_annotated_image, format="bgr24")
 
-    while cap.isOpened() and not stop_button_pressed:
-        success, frame = cap.read()
-        if not success:
-            st.write("Video Capture Ended")
-            break
+def gesture_recognition(frame: av.VideoFrame) -> av.VideoFrame:
+    image = frame.to_ndarray(format="bgr24")
+    rgb_frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
-
-        detection_result = detector.detect(mp_image)
-        image_copy = np.copy(mp_image.numpy_view())
-        annotated_image = visualize(image_copy, detection_result)
-        rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
-
-        #show image at streamlit
-        frame_placeholder.image(annotated_image,channels="RGB")
-
-        if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-    st.subheader("Thanks for using this app!")
-
-def gesture_recognition(camera_index):
 
     mp_hands = mp.solutions.hands
     mp_drawing = mp.solutions.drawing_utils
@@ -145,66 +129,59 @@ def gesture_recognition(camera_index):
     options = vision.GestureRecognizerOptions(base_options=base_options)
     recognizer = vision.GestureRecognizer.create_from_options(options)
 
-    cap = cv2.VideoCapture(camera_index)
-    if not cap.isOpened():
-        st.subheader("camera not available")
-        st.caption("Please change the camera channel and try again")
-        ok_button = st.button("Ok")
-        return
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
 
-    stop_button_pressed = st.button("Stop camera", type="primary")
-    frame_placeholder = st.empty()
+    recognition_result = recognizer.recognize(mp_image)
 
-    gesture_placeholder = st.empty()
+    annotated_image = np.copy(mp_image.numpy_view())
 
-    while cap.isOpened() and not stop_button_pressed:
-
-        success, frame = cap.read()
-        if not success:
-            break
-
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
-
-        recognition_result = recognizer.recognize(mp_image)
-
-        annotated_image = np.copy(mp_image.numpy_view())
+    if recognition_result.gestures:
+        top_gesture = recognition_result.gestures[0][0]
+    else:
+        top_gesture = None
+    multi_hand_landmarks = recognition_result.hand_landmarks
     
-        if recognition_result.gestures:
-            top_gesture = recognition_result.gestures[0][0]
-        else:
-            top_gesture = None
-        multi_hand_landmarks = recognition_result.hand_landmarks
 
-        for hand_landmarks in multi_hand_landmarks:
+    for hand_landmarks in multi_hand_landmarks:
 
-            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-            hand_landmarks_proto.landmark.extend([
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-            ])
-            mp_drawing.draw_landmarks(
-            annotated_image,
-            hand_landmarks_proto,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+        ])
+        mp_drawing.draw_landmarks(
+        annotated_image,
+        hand_landmarks_proto,
+        mp_hands.HAND_CONNECTIONS,
+        mp_drawing_styles.get_default_hand_landmarks_style(),
+        mp_drawing_styles.get_default_hand_connections_style())
 
-        #rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+    rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
 
-        #cv2.imshow('image', rgb_annotated_image)
-        frame_placeholder.image(annotated_image,channels="RGB")
-        if top_gesture:
-            gesture_placeholder.subheader(f"{top_gesture.category_name}: {top_gesture.score:.2f}" )
-        else:
-            gesture_placeholder.subheader("No gesture detected")
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    cap.release()
-    cv2.destroyAllWindows()
-    st.subheader("Thanks for using this app!")
+    if top_gesture:
+        label = top_gesture.category_name
+        score = top_gesture.score
+    else:
+        label = "No gesture detected"
+        score = None
 
-def face_landmarks(camera_index):
+    detections = [
+        Detection(
+            label=label,
+            score=score
+            )
+    ]
+    # if top_gesture:
+    #     gesture_placeholder.subheader(f"{top_gesture.category_name}: {top_gesture.score:.2f}" )
+    #     st.write(f"{top_gesture.category_name}: {top_gesture.score:.2f}")
+    # else:
+    #     gesture_placeholder.subheader("No gesture detected")
+
+    result_queue.put(detections)
+    return av.VideoFrame.from_ndarray(rgb_annotated_image, format="bgr24")
+
+def face_landmarks(frame: av.VideoFrame) -> av.VideoFrame:
+    image = frame.to_ndarray(format="bgr24")
+    rgb_frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     model_path = 'models/face_landmarker.task'
 
@@ -215,41 +192,22 @@ def face_landmarks(camera_index):
                                         num_faces=1)
     detector = vision.FaceLandmarker.create_from_options(options)
 
-    cap = cv2.VideoCapture(camera_index)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+    
+    detection_result = detector.detect(mp_image)
 
-    if not cap.isOpened():
-        st.subheader("camera not available")
-        st.caption("Please change the camera channel and try again")
-        ok_button = st.button("Ok")
-        return
+    image_copy = np.copy(mp_image.numpy_view())
+    annotated_image = draw_landmarks_on_image(image_copy, detection_result)
 
+    rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
 
-    stop_button_pressed = st.button("Stop camera", type="primary")
-    frame_placeholder = st.empty()
+    return av.VideoFrame.from_ndarray(rgb_annotated_image, format="bgr24")
 
-    while cap.isOpened() and not stop_button_pressed:
-        success, frame = cap.read()
-        if not success:
-            st.write("Video Capture Ended")
-            break
+class Detection(NamedTuple):
+    label: str
+    score: float
 
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
-
-        detection_result = detector.detect(mp_image)
-        image_copy = np.copy(mp_image.numpy_view())
-        annotated_image = draw_landmarks_on_image(image_copy, detection_result)
-
-        frame_placeholder.image(annotated_image,channels="RGB")
-
-        if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-    st.subheader("Thanks for using this app!")
-
-
+result_queue: "queue.Queue[List[Detection]]" = queue.Queue()
 
 
 def main():
@@ -257,19 +215,8 @@ def main():
     st.title("Vision ML Projects")
     st.caption("Allow camera access to use this app.")
 
-
-    col1, col2 = st.columns(2) 
-
-    with col1:
-        title_project = st.empty()
-        open_camera = st.empty()
-        open_camera_pressed = open_camera.button("Open Camera", type="primary")
-
-    with col2:
-        camera_channel = st.empty()
-        camera_index = camera_channel.selectbox("Switch camera channel", [1, 2, 3], index=0)
-
-
+    title_project = st.empty()
+    
 
     with st.sidebar:
 
@@ -281,22 +228,32 @@ def main():
 
     if selected == "Object Detection":
         title_project.subheader("Object Detection")
-        if open_camera_pressed:
-            open_camera.empty()
-            camera_channel.selectbox("Switch camera channel", [1, 2, 3], index=camera_index-1, disabled=True, key="camera_channel_disabled")
-            object_detection(camera_index-1)
+        webrtc_ctx = webrtc_streamer(
+            key="object-detection", 
+            media_stream_constraints={"video": True, "audio": False},
+            video_frame_callback=object_detection,
+        )
     if selected == "Hand Tracking":
         title_project.subheader("Hand Tracking")
-        if open_camera_pressed:
-            open_camera.empty()
-            camera_channel.selectbox("Switch camera channel", [1, 2, 3], index=camera_index-1, disabled=True, key="camera_channel_disabled")
-            gesture_recognition(camera_index-1)
+
+        webrtc_ctx = webrtc_streamer(
+            key="object-detection", 
+            media_stream_constraints={"video": True, "audio": False},
+            video_frame_callback=gesture_recognition,
+        )
+        if webrtc_ctx.state.playing:
+            labels_placeholder = st.empty()
+            while True:
+                result = result_queue.get()
+                labels_placeholder.table(result)
+
     if selected == "Face Landmarkers":
         title_project.subheader("Face Landmarkers")
-        if open_camera_pressed:
-            open_camera.empty()
-            camera_channel.selectbox("Switch camera channel", [1, 2, 3], index=camera_index-1, disabled=True, key="camera_channel_disabled")
-            face_landmarks(camera_index-1)
+        webrtc_ctx = webrtc_streamer(
+            key="object-detection", 
+            media_stream_constraints={"video": True, "audio": False},
+            video_frame_callback=face_landmarks,
+        )
 
 
 
